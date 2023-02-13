31ccde0c28db0881100b27ee5b37cba8adc0e2bc
DevExpressExampleBot <fakeEmail>
2023-02-13 17:50:15 +0400

    Source auto update [skip ci]

--- VB/CardFormatRulesSample/Form1.Designer.vb
+++ VB/CardFormatRulesSample/Form1.Designer.vb
@@ -1,56 +1,55 @@
-﻿Namespace CardFormatRulesSample
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace CardFormatRulesSample
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.dashboardDesigner1 = New DevExpress.DashboardWin.DashboardDesigner()
-			CType(Me.dashboardDesigner1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' dashboardDesigner1
-			' 
-			Me.dashboardDesigner1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.dashboardDesigner1.Location = New System.Drawing.Point(0, 0)
-			Me.dashboardDesigner1.Name = "dashboardDesigner1"
-			Me.dashboardDesigner1.Size = New System.Drawing.Size(800, 450)
-			Me.dashboardDesigner1.TabIndex = 0
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(800, 450)
-			Me.Controls.Add(Me.dashboardDesigner1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.dashboardDesigner1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.dashboardDesigner1 = New DevExpress.DashboardWin.DashboardDesigner()
+            CType((Me.dashboardDesigner1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' dashboardDesigner1
+            ' 
+            Me.dashboardDesigner1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.dashboardDesigner1.Location = New System.Drawing.Point(0, 0)
+            Me.dashboardDesigner1.Name = "dashboardDesigner1"
+            Me.dashboardDesigner1.Size = New System.Drawing.Size(800, 450)
+            Me.dashboardDesigner1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(800, 450)
+            Me.Controls.Add(Me.dashboardDesigner1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.dashboardDesigner1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private dashboardDesigner1 As DevExpress.DashboardWin.DashboardDesigner
-	End Class
+#End Region
+        Private dashboardDesigner1 As DevExpress.DashboardWin.DashboardDesigner
+    End Class
 End Namespace
-
--- VB/CardFormatRulesSample/Form1.vb
+++ VB/CardFormatRulesSample/Form1.vb
@@ -1,81 +1,80 @@
-﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon
 Imports System.Drawing
 Imports System.Windows.Forms
 
 Namespace CardFormatRulesSample
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-			dashboardDesigner1.CreateRibbon()
-			dashboardDesigner1.Dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
-			Me.WindowState = FormWindowState.Maximized
-			Dim cardItem As CardDashboardItem = CType(dashboardDesigner1.Dashboard.Items("cardDashboardItem1"), CardDashboardItem)
-			Dim layout As New CardCompactLayoutTemplate()
-			Dim layout2 As New CardCompactLayoutTemplate()
-			cardItem.Cards(0).LayoutTemplate = layout
-			cardItem.Cards(1).LayoutTemplate = layout2
+    Public Partial Class Form1
+        Inherits Form
 
-			AddRulesToTheFirstCard(cardItem.FormatRules, cardItem.Cards(0))
-			AddRulesToTheSecondCard(cardItem.FormatRules, cardItem.Cards(1))
-			AddCommonRule(cardItem.FormatRules, cardItem.SeriesDimensions(0))
-		End Sub
-		Public Sub AddCommonRule(ByVal formatRules As CardItemFormatRuleCollection, ByVal dataItem As DataItem)
-			' Applies a light-green background color to cards whose actual value is equal to the set condition.
-			Dim backColorStyleRule As New CardItemFormatRule(dataItem)
-			backColorStyleRule.ApplyToLayoutElement = CardFormatRuleLayoutElement.ActualValue
-			Dim valueCondition As New FormatConditionValue(DashboardFormatCondition.Equal, "Chain")
-			valueCondition.StyleSettings = New AppearanceSettings(Color.LightGreen)
-			backColorStyleRule.Condition = valueCondition
-			formatRules.Add(backColorStyleRule)
-		End Sub
-		Public Sub AddRulesToTheFirstCard(ByVal formatRules As CardItemFormatRuleCollection, ByVal card As Card)
-			' Applies a predefined range of colors to cards.
-			Dim gradientRule As New CardItemDeltaFormatRule()
-			gradientRule.DeltaValueType = DeltaValueType.AbsoluteVariation
-			gradientRule.Card = card
-			Dim rangeCondition = New FormatConditionRangeGradient(FormatConditionRangeGradientPredefinedType.BlueWhiteGreen)
-			gradientRule.Condition = rangeCondition
-			formatRules.Add(gradientRule)
+        Public Sub New()
+            InitializeComponent()
+            dashboardDesigner1.CreateRibbon()
+            dashboardDesigner1.Dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
+            WindowState = FormWindowState.Maximized
+            Dim cardItem As CardDashboardItem = CType(dashboardDesigner1.Dashboard.Items("cardDashboardItem1"), CardDashboardItem)
+            Dim layout As CardCompactLayoutTemplate = New CardCompactLayoutTemplate()
+            Dim layout2 As CardCompactLayoutTemplate = New CardCompactLayoutTemplate()
+            cardItem.Cards(0).LayoutTemplate = layout
+            cardItem.Cards(1).LayoutTemplate = layout2
+            AddRulesToTheFirstCard(cardItem.FormatRules, cardItem.Cards(0))
+            AddRulesToTheSecondCard(cardItem.FormatRules, cardItem.Cards(1))
+            AddCommonRule(cardItem.FormatRules, cardItem.SeriesDimensions(0))
+        End Sub
 
-			' Applies the bold dark-blue font style to the cards' actual value that is between the range of condition values.
-			Dim fontStyleRule As New CardItemFormatRule()
-			fontStyleRule.DataItem = card.ActualValue
-			fontStyleRule.ApplyToLayoutElement = CardFormatRuleLayoutElement.ActualValue
-			Dim valueCondition As New FormatConditionValue(DashboardFormatCondition.Between, 100000, 700000)
-			valueCondition.StyleSettings = New AppearanceSettings(Color.DarkBlue, FontStyle.Bold)
-			fontStyleRule.Condition = valueCondition
-			formatRules.Add(fontStyleRule)
+        Public Sub AddCommonRule(ByVal formatRules As CardItemFormatRuleCollection, ByVal dataItem As DataItem)
+            ' Applies a light-green background color to cards whose actual value is equal to the set condition.
+            Dim backColorStyleRule As CardItemFormatRule = New CardItemFormatRule(dataItem)
+            backColorStyleRule.ApplyToLayoutElement = CardFormatRuleLayoutElement.ActualValue
+            Dim valueCondition As FormatConditionValue = New FormatConditionValue(DashboardFormatCondition.Equal, "Chain")
+            valueCondition.StyleSettings = New AppearanceSettings(Color.LightGreen)
+            backColorStyleRule.Condition = valueCondition
+            formatRules.Add(backColorStyleRule)
+        End Sub
 
-			' Applies full gray star icons to cards whose percentage variation value is greater than the set condition value.
-			Dim iconRule As New CardItemDeltaFormatRule(card, DeltaValueType.PercentVariation, CardFormatRuleLayoutElement.Title)
-			Dim valueCondition2 As New FormatConditionValue(DashboardFormatCondition.Greater, 0.1)
-			valueCondition2.StyleSettings = New IconSettings(FormatConditionIconType.RatingFullGrayStar)
-			iconRule.Condition = valueCondition2
-			formatRules.Add(iconRule)
-		End Sub
-		Public Sub AddRulesToTheSecondCard(ByVal formatRules As CardItemFormatRuleCollection, ByVal card As Card)
-			' Applies a predefined range of colors to cards.
-			Dim rangeRule As New CardItemDeltaFormatRule()
-			rangeRule.DeltaValueType = DeltaValueType.AbsoluteVariation
-			rangeRule.Card = card
-			Dim rangeCondition = New FormatConditionRangeSet(FormatConditionRangeSetPredefinedType.ColorsPaleRedOrangeYellowGreenBlue)
-			rangeRule.Condition = rangeCondition
-			formatRules.Add(rangeRule)
+        Public Sub AddRulesToTheFirstCard(ByVal formatRules As CardItemFormatRuleCollection, ByVal card As Card)
+            ' Applies a predefined range of colors to cards.
+            Dim gradientRule As CardItemDeltaFormatRule = New CardItemDeltaFormatRule()
+            gradientRule.DeltaValueType = DeltaValueType.AbsoluteVariation
+            gradientRule.Card = card
+            Dim rangeCondition = New FormatConditionRangeGradient(FormatConditionRangeGradientPredefinedType.BlueWhiteGreen)
+            gradientRule.Condition = rangeCondition
+            formatRules.Add(gradientRule)
+            ' Applies the bold dark-blue font style to the cards' actual value that is between the range of condition values.
+            Dim fontStyleRule As CardItemFormatRule = New CardItemFormatRule()
+            fontStyleRule.DataItem = card.ActualValue
+            fontStyleRule.ApplyToLayoutElement = CardFormatRuleLayoutElement.ActualValue
+            Dim valueCondition As FormatConditionValue = New FormatConditionValue(DashboardFormatCondition.Between, 100000, 700000)
+            valueCondition.StyleSettings = New AppearanceSettings(Color.DarkBlue, FontStyle.Bold)
+            fontStyleRule.Condition = valueCondition
+            formatRules.Add(fontStyleRule)
+            ' Applies full gray star icons to cards whose percentage variation value is greater than the set condition value.
+            Dim iconRule As CardItemDeltaFormatRule = New CardItemDeltaFormatRule(card, DeltaValueType.PercentVariation, CardFormatRuleLayoutElement.Title)
+            Dim valueCondition2 As FormatConditionValue = New FormatConditionValue(DashboardFormatCondition.Greater, 0.1)
+            valueCondition2.StyleSettings = New IconSettings(FormatConditionIconType.RatingFullGrayStar)
+            iconRule.Condition = valueCondition2
+            formatRules.Add(iconRule)
+        End Sub
 
-			' Applies the underlined red font style to the title of cards whose absolute variation value is less than the condition value.
-			Dim fontStyleUnderlinedRule As New CardItemDeltaFormatRule(card, DeltaValueType.AbsoluteVariation, CardFormatRuleLayoutElement.Title)
-			Dim valueCondition As New FormatConditionValue(DashboardFormatCondition.Less, 0)
-			valueCondition.StyleSettings = New AppearanceSettings(Color.Red, FontStyle.Underline)
-			fontStyleUnderlinedRule.Condition = valueCondition
-			formatRules.Add(fontStyleUnderlinedRule)
-
-			' Applies the positive-negative icon range to cards.
-			Dim deltaIconRule As New CardItemDeltaFormatRule(card, DeltaValueType.AbsoluteVariation, CardFormatRuleLayoutElement.Indicator)
-			Dim iconRangeCondition = New FormatConditionRangeSet(FormatConditionRangeSetPredefinedType.PositiveNegative3)
-			deltaIconRule.Condition = iconRangeCondition
-			formatRules.Add(deltaIconRule)
-		End Sub
-	End Class
+        Public Sub AddRulesToTheSecondCard(ByVal formatRules As CardItemFormatRuleCollection, ByVal card As Card)
+            ' Applies a predefined range of colors to cards.
+            Dim rangeRule As CardItemDeltaFormatRule = New CardItemDeltaFormatRule()
+            rangeRule.DeltaValueType = DeltaValueType.AbsoluteVariation
+            rangeRule.Card = card
+            Dim rangeCondition = New FormatConditionRangeSet(FormatConditionRangeSetPredefinedType.ColorsPaleRedOrangeYellowGreenBlue)
+            rangeRule.Condition = rangeCondition
+            formatRules.Add(rangeRule)
+            ' Applies the underlined red font style to the title of cards whose absolute variation value is less than the condition value.
+            Dim fontStyleUnderlinedRule As CardItemDeltaFormatRule = New CardItemDeltaFormatRule(card, DeltaValueType.AbsoluteVariation, CardFormatRuleLayoutElement.Title)
+            Dim valueCondition As FormatConditionValue = New FormatConditionValue(DashboardFormatCondition.Less, 0)
+            valueCondition.StyleSettings = New AppearanceSettings(Color.Red, FontStyle.Underline)
+            fontStyleUnderlinedRule.Condition = valueCondition
+            formatRules.Add(fontStyleUnderlinedRule)
+            ' Applies the positive-negative icon range to cards.
+            Dim deltaIconRule As CardItemDeltaFormatRule = New CardItemDeltaFormatRule(card, DeltaValueType.AbsoluteVariation, CardFormatRuleLayoutElement.Indicator)
+            Dim iconRangeCondition = New FormatConditionRangeSet(FormatConditionRangeSetPredefinedType.PositiveNegative3)
+            deltaIconRule.Condition = iconRangeCondition
+            formatRules.Add(deltaIconRule)
+        End Sub
+    End Class
 End Namespace
